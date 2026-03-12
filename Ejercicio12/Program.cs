33c2b0bc8c17473cdbdd8898ac77798ce6c2a7d5
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 21:18:27 -0600

    Se terminó el proyecto 12.

--- Ejercicio12/Program.cs
+++ Ejercicio12/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int edad;
+
+string MostrarEdad(int edad)
+{
+    return $"La edad ingresada es: {edad} años";
+}
+
+Console.Write("Ingresa tu edad: ");
+edad=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(MostrarEdad(edad));
