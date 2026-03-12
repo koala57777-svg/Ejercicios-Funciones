61f82dfb7a95b48b61451165fbc8189f3cee3cea
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 17:49:32 -0600

    Se terminó el proyecto 20: Aunque el ciclo no era necesario, lo hize para practicar.

--- Ejercicio20/Program.cs
+++ Ejercicio20/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿int diasTrabajados=1;
+double pagoDiario=1;
+
+string SalarioTotal(int a, double b)
+{
+    return $"El salario total del trabajador es: Q{a * b}";
+}
+
+do
+{
+    Console.Write("Ingrese el pago diario (Q): ");
+    pagoDiario = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (pagoDiario > 0)
+    {
+        Console.Write("Ingrese los días trabajados: ");
+        diasTrabajados = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        if (diasTrabajados > 0)
+        {
+            Console.WriteLine(SalarioTotal(diasTrabajados, pagoDiario));
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Dato inválido. Por favor ingrese un dato válido. ");
+            Console.WriteLine();
+            diasTrabajados = 1;
+            
+        }
+    }
+    else
+    {
+        Console.Write("Dato inválido. Por favor ingrese un dato válido. ");
+        Console.WriteLine();
+        pagoDiario = 1;
+        
+    }
+    Console.WriteLine();
+ 
+} while (pagoDiario > 0 || diasTrabajados > 0);
