95d16d33ab81e1e78720718b21739c8b2f6349b7
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 21:40:53 -0600

    Se terminó el proyecto 13.

--- Ejercicio13/Program.cs
+++ Ejercicio13/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int cantidadproductos;
+
+double productoingresado;
+
+double TotalCompra(int cantidadproductos)
+{
+    double sumatotal = 0;
+    for (int i=1; i<=cantidadproductos; i++)
+    {
+        Console.Write($"Ingrese el precio del producto {i} (Q): ");
+        productoingresado=double.Parse(Console.ReadLine());
+        sumatotal += productoingresado;
+        Console.WriteLine();
+    }
+    return sumatotal;
+}
+
+Console.Write("Cantidad de productos que desea ingresar: ");
+cantidadproductos=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine();
+double resultado=TotalCompra(cantidadproductos);
+Console.WriteLine();
+Console.WriteLine($"El total de las compras es: Q{resultado}");
