a19e1aee46322c52b02ddc12819e5123604ba7ef
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:25:34 -0600

    Se terminó el proyecto 8.

--- Ejercicio8/Program.cs
+++ Ejercicio8/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿double a, b, c;
+
+double Promedio(double a, double b, double c)
+{
+    return (a + b + c) / 3;
+}
+
+Console.WriteLine("Ingrese el primer número: ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese el segundo número: ");
+Console.WriteLine();
+b = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese el tercer número: ");
+Console.WriteLine();
+c = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"El promedioo de los número es: {Promedio(a,b,c)}");
