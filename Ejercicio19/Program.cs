36b9507ea7d451372a6cac9c3d472461c42c20a6
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 17:00:17 -0600

    Se terminó el proyecto 19.

--- Ejercicio19/Program.cs
+++ Ejercicio19/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿double calificacion;
+string Nombreestudiante;
+string PromedioEstudiante(string a)
+{
+    double sumacalficaciones = 0;
+
+    for (int i=1; i<=3; i++)
+    {
+        Console.Write($"Ingrese la nota {i} del estudiante {Nombreestudiante}: ");
+        calificacion=double.Parse( Console.ReadLine() );
+        Console.WriteLine();
+        if ( calificacion>=0 && calificacion <= 100)
+        {
+            sumacalficaciones += calificacion;
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Dato inválido. Por favor ingrese datos válidos");
+            i = i - 1;
+            Console.WriteLine();
+        }
+    }
+    return $"El promedio de las calificaciones del estudiante {Nombreestudiante} es: {sumacalficaciones/3}";
+}
+
+Console.Write("Ingrese el nombre del estudiante: ");
+Nombreestudiante= Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine(PromedioEstudiante(Nombreestudiante));
