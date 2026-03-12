51b4230c11ed27530d52eaf89700bde3b5b7ae53
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 16:23:10 -0600

    Se terminó el proyecto 17. Aprendí usos interesantes del ciclo for, y un poco de como funcionan (valga la redundancia) las funciones. Muy bonito esto de las funciones.

--- Ejercicio17/Program.cs
+++ Ejercicio17/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿int cantidadEstudiantes;
+string PromedioGrupal(int cantidadEstudiantes)
+{
+    double suma = 0;
+    double notaIngresasda = 0;
+
+    for (int i=1; i<=cantidadEstudiantes; i++)
+    {
+        Console.Write($"Ingrese la nota del estudiante {i}: ");
+        notaIngresasda=double.Parse( Console.ReadLine() );
+        Console.WriteLine();
+        if (notaIngresasda > 0 && notaIngresasda <= 100)
+        {
+            suma += notaIngresasda;
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Nota ingresada no válida. Por favor ingreses datos válidos.");
+            i = i - 1;
+            Console.WriteLine();
+        }
+    }
+    return $"El promedio grupal es de: {suma/cantidadEstudiantes}";
+}
+
+Console.Write("Cantidad de estudiantes que desea ingresar: ");
+cantidadEstudiantes = int.Parse(Console.ReadLine());
+Console.WriteLine(PromedioGrupal(cantidadEstudiantes));
