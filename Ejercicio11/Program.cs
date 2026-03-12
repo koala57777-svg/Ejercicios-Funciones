4ada84b2a018da3346054c60ba3dde12e437aa30
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:53:45 -0600

    Se terminó el proyecto 11

--- Ejercicio11/Program.cs
+++ Ejercicio11/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿double a;
+
+double PesoLibras(double a)
+{
+    return (a*2.2);
+}
+
+Console.WriteLine("Ingrese el peso (Kg): ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (a < 0)
+{
+    Console.WriteLine("Dato inválido. Por favor ingresar datos positivos.");
+}
+else
+{
+    Console.WriteLine($"{a} kg son: {PesoLibras(a)} lb");
+}
