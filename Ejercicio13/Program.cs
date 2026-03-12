2aaacc10307bfd1a759b6be1ce66666f81059e3d
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 16:32:33 -0600

    13.1: Se mejoró gracias a lo aprendido en el ejercicio 17.

--- Ejercicio13/Program.cs
+++ Ejercicio13/Program.cs
@@ -2,24 +2,30 @@
 
 double productoingresado;
 
-double TotalCompra(int cantidadproductos)
+string TotalCompra(int cantidadproductos)
 {
     double sumatotal = 0;
     for (int i=1; i<=cantidadproductos; i++)
     {
         Console.Write($"Ingrese el precio del producto {i} (Q): ");
         productoingresado=double.Parse(Console.ReadLine());
-        sumatotal += productoingresado;
         Console.WriteLine();
+        if (productoingresado > 0)
+        {
+            sumatotal += productoingresado;
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Dato invalido. Por favor ingrese datos válidos"); ;
+            i = i - 1;
+            Console.WriteLine();
+        }
     }
-    return sumatotal;
+    return $"La suma total es de: Q{sumatotal}";
 }
 
 Console.Write("Cantidad de productos que desea ingresar: ");
 cantidadproductos=int.Parse(Console.ReadLine());
 Console.WriteLine();
-Console.WriteLine();
-double resultado=TotalCompra(cantidadproductos);
-Console.WriteLine();
-Console.WriteLine($"El total de las compras es: Q{resultado}");
-Console.WriteLine();
+Console.WriteLine(TotalCompra(cantidadproductos));
