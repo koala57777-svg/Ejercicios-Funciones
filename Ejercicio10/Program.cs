3a350f76bd417c2267d20e72e7ee80b2e1154b13
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:43:44 -0600

    Se terminó el proyecto 10.

--- Ejercicio10/Program.cs
+++ Ejercicio10/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿double a;
+
+double CuboNumero(double a)
+{
+    return a * a * a;
+}
+
+Console.WriteLine("Ingrese un número: ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"El cubo del número es: {CuboNumero(a)}");
