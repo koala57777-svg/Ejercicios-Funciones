ee5f3f19e080adfd0e7733af20a8e7aacb774259
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 15:57:30 -0600

    Se terminó el proyecto 16.

--- Ejercicio16/Program.cs
+++ Ejercicio16/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿double minutos;
+
+string ConvertirMinutos(double a)
+{
+    return $"El equivalente de {a} minutos es: {a/60} horas";
+}
+
+Console.Write("Ingrese la cantidad de minutos: ");
+minutos=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (minutos < 0)
+{
+    Console.WriteLine("Dato inválidos. Por favor ingrese datos positivos");
+}
+else
+{
+    Console.WriteLine(ConvertirMinutos(minutos));
+}
