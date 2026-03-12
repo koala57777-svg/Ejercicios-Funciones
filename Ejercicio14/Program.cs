0099f4e793c4de94a7fb88fc70c96ef69b5067dc
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 15:35:45 -0600

    Se terminó el proyecto 14.

--- Ejercicio14/Program.cs
+++ Ejercicio14/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿double distancia, tiempo;
+
+double CalcularVelocidad(double a, double b)
+{
+    return a / b;
+}
+
+Console.Write("Ingrese la distancia recorrida (m): ");
+distancia=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Ingrese el tiempo empleado (s): ");
+tiempo = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (distancia<0 || tiempo <= 0)
+{
+    Console.WriteLine("Datos inválidos. Por favor ingrese datos válidos.");
+}
+else
+{
+    Console.WriteLine($"La velocidad promedio es de: {CalcularVelocidad(distancia,tiempo)} m/s");
+}
