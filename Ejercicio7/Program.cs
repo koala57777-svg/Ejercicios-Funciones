d5d81c890b9131f3dd43102c1cef841ec0ed0e0e
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:19:33 -0600

    Se terminó el proyecto 7.

--- Ejercicio7/Program.cs
+++ Ejercicio7/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿double a, b;
+double PrecioFinal(double a, double b)
+{
+    double descuento = a * (b / 100);
+    return a - descuento;
+}
+
+Console.WriteLine("Ingrese el precio del producto: ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese el porcentaje de descuento del producto: ");
+Console.WriteLine();
+b=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (a < 0 || b < 0)
+{
+    Console.WriteLine("Datos inválidos. Por favor ingrese valores positivos.");
+}
+else
+{
+    Console.WriteLine($"El precio final del producto es de: {PrecioFinal(a,b)}");
+}
