d6c58392b7dfed3ff08ba87eb7dba4f49155c0ce
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 19:46:19 -0600

    Se terminó el proyecto 1.

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine();
+﻿using System.Net.WebSockets;
+
+double a, b;
+double Suma(double a, double b)
+{
+    return a + b;
+}
+
+Console.WriteLine("Ingrese el primer número: ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese el segundo número: ");
+Console.WriteLine();
+b=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"La suma de los números es: {Suma(a,b)}");
