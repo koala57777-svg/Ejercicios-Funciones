b83635b9e2eccbe721e3e6b8725dc5c949800734
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:02:08 -0600

    Se terminó el proyecto 4.

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿double a;
+double RadioCirculo(double a)
+{
+    return Math.PI * (a * a);
+
+}
+
+Console.WriteLine("Ingrse el radio del círculo: ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (a < 0)
+{
+    Console.WriteLine("Dato inválido. Ingrese un valor positivo.");
+}
+else
+{
+    Console.WriteLine($"El radio del círculo es de: {RadioCirculo(a)} unidades cuadradas.");
+}
