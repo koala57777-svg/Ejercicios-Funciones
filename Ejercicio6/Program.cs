0eb0232d11a16cd71aa72d94f0afcd3ba4c3474c
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:13:37 -0600

    Se terminó el proyecto 6.

--- Ejercicio6/Program.cs
+++ Ejercicio6/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿double a;
+double GradosFahrenheit(double a)
+{
+    return (a * 9 / 5) + 32;
+}
+
+Console.WriteLine("Ingrese los grados (Celsius): ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"{a} grados Celsius son: {GradosFahrenheit(a)} grados Fahrenheit.");
