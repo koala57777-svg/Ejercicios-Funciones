bdd13a8bf7eb0f6b5d20ba5b03ac2e0586fca652
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 15:44:28 -0600

    Se terminó el proyecto 15.

--- Ejercicio15/Program.cs
+++ Ejercicio15/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿double baseTriangulo, alturaTriangulo;
+
+double AreaTriangulo(double a, double b)
+{
+    return ((0.5) * (a * b));
+}
+
+Console.Write("Ingrese la base del triángulo: ");
+baseTriangulo=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Ingrese la altura del triángulo: ");
+alturaTriangulo = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (baseTriangulo<0 || alturaTriangulo < 0)
+{
+    Console.WriteLine("Datos inválidos. Por favor ingresar datos válidos.");
+}
+else
+{
+    Console.WriteLine($"El área del triángulo es de: {AreaTriangulo(baseTriangulo, alturaTriangulo)} unidades cuadradas");
+}
