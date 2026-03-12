e67bbf57188dcb0e32ba93fa82f81de2726c1c1f
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 19:50:29 -0600

    Se terminó el proyecto 2.

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Net.WebSockets;
+
+double a;
+double Cuadrado(double a)
+{
+    return a * a;
+}
+
+Console.WriteLine("Ingrese un número: ");
+Console.WriteLine();
+a= double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"El cuadrado del número es: {Cuadrado(a)}");
