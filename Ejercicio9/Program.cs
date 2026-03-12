dfcb260782f47f2131a015e68bac48f6e86e2b7d
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:39:27 -0600

    Se terminó el proyecto 9.

--- Ejercicio9/Program.cs
+++ Ejercicio9/Program.cs
@@ -13,3 +13,11 @@ Console.WriteLine("Ingrese las horas trabajadas: ");
 Console.WriteLine();
 b = double.Parse(Console.ReadLine());
 Console.WriteLine();
+if (a<0 || b < 0)
+{
+    Console.WriteLine("Datos inválidos. Por favor ingrese datos positivos.");
+}
+else
+{
+    Console.WriteLine($"El salario total es de: Q{SalarioTotal(a,b)}");
+}
