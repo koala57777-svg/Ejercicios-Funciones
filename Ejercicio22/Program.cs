ce4a99637a7c6110057f8776cbd2366bfa62acbc
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 17:55:56 -0600

    Se terminó el proyecto 22.

--- Ejercicio22/Program.cs
+++ Ejercicio22/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿void SaludoPersonalizado()
+{
+    Console.Write("Ingrese su nombre: ");
+    string Nombre= Console.ReadLine();
+    Console.WriteLine();
+    Console.WriteLine($"Hola {Nombre}, ¡Espero que tengas un buen día!");
+}
+
+SaludoPersonalizado();
