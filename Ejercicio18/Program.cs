dec46c29c4efe0b4c18cd28913ea03dc61ed2721
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 16:43:51 -0600

    Se terminó el proyecto 18.

--- Ejercicio18/Program.cs
+++ Ejercicio18/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿int cantidadProductos;
+
+string TotalFactura(int a)
+{
+    double sumatotal = 0;
+    double precioproducto = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        Console.Write($"Ingrese el precio del producto {i} (Q):");
+        precioproducto = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        if (precioproducto > 0)
+        {
+            sumatotal += precioproducto;
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Dato inválido. Por favor ingrese datos válidos");
+            i = i - 1;
+            Console.WriteLine();
+        }
+    }
+    return $"El total de la factura es de: Q{sumatotal}";
+}
+
+Console.Write("Cantidad de productos que desea ingresar precio: ");
+cantidadProductos=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(TotalFactura(cantidadProductos));
+
