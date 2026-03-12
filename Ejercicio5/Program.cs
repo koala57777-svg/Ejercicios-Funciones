1fc4df0c46d9e878b7af2d835f4e822a1dda0ce6
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 20:07:44 -0600

    Se terminó el proyecto 5.

--- Ejercicio5/Program.cs
+++ Ejercicio5/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿double a, b;
+double AreaRectangulo(double a, double b)
+{
+    return a * b;
+}
+
+Console.WriteLine("Ingrese la altura del rectángulo: ");
+Console.WriteLine();
+a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese la base del rectángulo: ");
+Console.WriteLine();
+b=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (a<0 && b<0)
+{
+    Console.WriteLine("Datos inválidos. Por favor ingrese valores positivos.");
+}
+else
+{
+    Console.WriteLine($"El área del rectangulo es de: {AreaRectangulo(a,b)} unidades cuadradas.");
+}
