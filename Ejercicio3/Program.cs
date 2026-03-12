cfc4d34590e5d6921cb762de54b30ac7b84a7a6c
Gerber Ramírez <koala57777@gmail.com>
2026-03-11 19:54:29 -0600

    Se terminó el proyecto 3.

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine();
+﻿double a;
+double Doble(double a)
+{
+    return a * 2;
+}
+
+Console.WriteLine("Ingrese un número: ");
+Console.WriteLine();
+a=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"El doble del número es: {Doble(a)}");
