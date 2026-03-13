4aee7f6521a1d22786c0b6249c7f36452b8d6489
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 18:33:02 -0600

    Se terminó el proyecto 27.

--- Ejercicio27/Program.cs
+++ Ejercicio27/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿void DescuentoProducto()
+{
+    Console.Write("Ingrese el precio del producto (Q): ");
+    double precioProducto=double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (precioProducto > 0)
+    {
+        double descuento = precioProducto * 0.1;
+        Console.WriteLine($"El precio del producto con descuento es: Q{precioProducto-descuento}");
+    }
+    else
+    {
+        Console.WriteLine("Valor inválido. Por favor ingrese un valor válido.");
+        Console.WriteLine();
+        DescuentoProducto();
+    }
+}
+
+DescuentoProducto();
