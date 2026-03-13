2707df2a1603147a2f124863250955d5b75c7e41
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 18:25:52 -0600

    Se terminó el proyecto 26.

--- Ejercicio26/Program.cs
+++ Ejercicio26/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿void YearNacimientoAproximado()
+{
+    Console.Write("Ingrese su edad: ");
+    int edad=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (edad >= 0)
+    {
+        Console.WriteLine($"Su año de nacimiento aproximado es: {2026-edad}");
+    }
+    else
+    {
+        Console.WriteLine("Edad inválida. Por favor ingrese una edad válida");
+        Console.WriteLine();
+        YearNacimientoAproximado();
+    }
+}
+
+YearNacimientoAproximado();
