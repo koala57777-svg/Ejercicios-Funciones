62f9c2e599c478afc8a90f929d3e92fdffa556b4
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 18:18:59 -0600

    Se terminó el proyecto 25.

--- Ejercicio25/Program.cs
+++ Ejercicio25/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿void AreaRectangulo()
+{
+    Console.Write("Ingrese la base del rectángulo: ");
+    double baseRectangulo=double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (baseRectangulo > 0)
+    {
+        Console.Write("Ingrese la base del rectángulo: ");
+        double alturaRectangulo = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        if (alturaRectangulo > 0)
+        {
+            Console.WriteLine($"El área del rectángulo es: {baseRectangulo*alturaRectangulo} unidades cuadradas");
+        }
+        else
+        {
+            Console.WriteLine("Dato inválido. Ingrese dato válido");
+            Console.WriteLine();
+            AreaRectangulo();
+        }
+    }
+    else
+    {
+        Console.WriteLine("Dato inválido. Ingrese dato válido");
+        Console.WriteLine();
+        AreaRectangulo();
+    }
+}
+
+AreaRectangulo();  
