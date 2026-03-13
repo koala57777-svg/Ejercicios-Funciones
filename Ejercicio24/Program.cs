7eb4a634fec0b0306c1c0b86aff59b6a5feed86b
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 18:10:23 -0600

    Se terminó el proyecto 24: Aprendí una forma "creativa" de que si el número no es valido entonces que el programa se repita asi mismo.

--- Ejercicio24/Program.cs
+++ Ejercicio24/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿void AreaCirculo()
+{
+    Console.Write("Ingrese el radio del círculo: ");
+    double radiocirculo=double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (radiocirculo >= 0)
+    {
+        Console.WriteLine($"El área del círculo es: {Math.PI * radiocirculo * radiocirculo} unidades cuadradas");
+    }
+    else
+    {
+        Console.WriteLine("Ingrese datos váidos. No existen longitudes negativas.");
+        Console.WriteLine();
+        AreaCirculo();
+    }
+}
+
+AreaCirculo();
