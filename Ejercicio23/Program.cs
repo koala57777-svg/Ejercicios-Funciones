984f7679f958cfbc59f07e1b48aa8b35d31b85dd
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 18:00:16 -0600

    Se terminó el proyecto 23.

--- Ejercicio23/Program.cs
+++ Ejercicio23/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿void SumaDosNumeros()
+{
+    Console.Write("Ingrese el primer número: ");
+    double numerouno=double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    Console.Write("Ingrese el segundo número: ");
+    double numerodos = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine($"La suma de los dos números es: {numerouno+numerodos}");
+}
+
+SumaDosNumeros();
