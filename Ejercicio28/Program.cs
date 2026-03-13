ea6b174d8c2bcfe2cfeb4de57a1a14ee5749472e
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 18:45:55 -0600

    Se terminó el proyecto 28.

--- Ejercicio28/Program.cs
+++ Ejercicio28/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿void PromedioEstudiante()
+{
+    double sumacalificaciones = 0;
+    double calificacion;
+
+    for(int i=1; i<=3; i++)
+    {
+        Console.Write($"Ingrese la nota número {i}: ");
+        calificacion=double.Parse( Console.ReadLine() );
+        Console.WriteLine();
+        if (calificacion>=0 && calificacion <= 100)
+        {
+            sumacalificaciones += calificacion;
+        }
+        else
+        {
+            Console.WriteLine($"Calificación inválida. Vuelva a ingresar la calificación número {i}");
+            Console.WriteLine();
+            i = i - 1;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"El promedio de las tres calificaciones del estudiante: {sumacalificaciones / 3}");
+}
+
+PromedioEstudiante();
