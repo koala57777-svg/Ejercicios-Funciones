9c130970207048f1734e07356bca91d740ec5b20
Gerber Ramírez <koala57777@gmail.com>
2026-03-12 18:53:26 -0600

    Se terminó el proyecto 29.

--- Ejercicio29/Program.cs
+++ Ejercicio29/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿void CantidadEstudiantesCurso()
+{
+    Console.Write("Ingrese la cantidad de estudiante que hay en el curso: ");
+    int cantidadEstudiante=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (cantidadEstudiante >= 0)
+    {
+        Console.WriteLine($"La cantidad de estudiante que hay en la clase es: {cantidadEstudiante} estudiante/s");
+    }
+    else
+    {
+        Console.WriteLine("Cantidad inválida. Por favor ingrese una cantidad válida");
+        Console.WriteLine();
+        CantidadEstudiantesCurso();
+    }
+}
+
+CantidadEstudiantesCurso();
